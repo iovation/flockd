2cc4b0e9a417af20eaafea703c98a3b3ea6d7c59
David E. Wheeler <david.wheeler@iovation.com>
2018-01-13 08:56:16 -0500

    Test file existence and content.

--- dirdb_test.go
+++ dirdb_test.go
@@ -1,9 +1,11 @@
 package dirdb
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/suite"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -56,4 +58,48 @@ func (s *TS) TestBasic() {
 	val, err = db.Get(key)
 	s.Nil(val, "Should again have no value")
 	s.EqualError(err, os.ErrNotExist.Error(), "Should have ErrNotExist error")
+}
+
+func (s *TS) TestFiles() {
+	db := s.db
+	key := "foo"
+	file := filepath.Join(db.root.dir, key)
+	s.fileNotExists(file)
+
+	err := db.Set(key, []byte("hello"))
+	s.Nil(err, "Should have no error on set")
+
+	s.FileExists(file, "File %q should now exist")
+	s.fileContains(file, []byte("hello"))
+}
+
+func (s *TS) fileContains(path string, data []byte) bool {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return s.Fail(
+			fmt.Sprintf("error when reading file(%q): %s", path, err),
+			"File %q should contain %q", path, string(data),
+		)
+	}
+	return s.Equal(
+		string(data), string(content),
+		"File %q should contain %q", path, string(data),
+	)
+}
+
+func (s *TS) fileNotExists(path string) bool {
+	_, err := os.Lstat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return s.True(true, "File %q should not exist", path)
+		}
+		return s.Fail(
+			fmt.Sprintf("error when running os.Lstat(%q): %s", path, err),
+			"File %q should not exist", path,
+		)
+	}
+	return s.Fail(
+		fmt.Sprintf("found file %q", path),
+		"File %q should not exist", path,
+	)
 }
