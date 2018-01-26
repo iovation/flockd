8c98fd9b7c0b4bf1f82d12a680b23723f6ea6d0a
David E. Wheeler <david.wheeler@iovation.com>
2018-01-26 17:58:03 -0500

    Improve docs.

--- flockd.go
+++ flockd.go
@@ -43,13 +43,14 @@ func New(dir string) (*DB, error) {
 	return &DB{root: root, tables: &sync.Map{}}, nil
 }
 
-// Table returns creates a table in the database. The table corresponds to a
-// subdirectory of the database root directory. Keys and values can be written
-// directly to the table. Pass a path created by filepath.Join to create a
-// deeper subdirectory. If the directory does not exist, it will be created.
-// Returns an error if the directory creation fails. If the table has been
-// created previously, it will be returned immediately without checking for the
-// existence of the directory on the file system.
+// Table creates a table in the database. The table corresponds to a
+// subdirectory of the database root directory. Its name will be the table name
+// plus the extension ".tbl". Keys and values can be written directly to the
+// table. Pass a path created by filepath.Join to create a deeper subdirectory.
+// If the directory does not exist, it will be created. Returns an error if the
+// directory creation fails. If the table has been created previously, it will
+// be returned immediately without checking for the existence of the directory
+// on the file system.
 func (db *DB) Table(subdir string) (*Table, error) {
 	if table, ok := db.tables.Load(subdir); ok {
 		return table.(*Table), nil
@@ -70,31 +71,32 @@ func newTable(path string) (*Table, error) {
 	return &Table{path: path}, nil
 }
 
-// Get returns the value for the key by reading the file named for key from the
-// root directory.
+// Get returns the value for the key by reading the file named for the key, plus
+// the extension ".kv", from the root directory.
 func (db *DB) Get(key string) ([]byte, error) {
 	return db.root.Get(key)
 }
 
-// Set sets the value for the key by writing it to the file named for key in the
-// root directory.
+// Set sets the value for the key by writing it to the file named for the key,
+// plus the extension ".kv", in the root directory.
 func (db *DB) Set(key string, val []byte) error {
 	return db.root.Set(key, val)
 }
 
-// Delete deletes the key and its value by deleting the file named for key in
-// the root directory.
+// Delete deletes the key and its value by deleting the file named for the key,
+// plus the extension ".kv", // in the root directory.
 func (db *DB) Delete(key string) error {
 	return db.root.Delete(key)
 }
 
-// Get returns the value for the key by reading the file named for key from the
-// table directory. The key must not contain a path separator character; if it
-// does, os.ErrInvalid will be returned. If the file does not exist,
-// os.ErrNotExist will be returned. For concurrency safetey, Get acquires a
-// shared file system lock on the file before reading its contents. If the file
-// has an exclusive lock on it, Get will spend up to a millisecond waiting for
-// the shared lock before returning a context.DeadlineExceeded error.
+// Get returns the value for the key by reading the file named for key, plus the
+// extension ".kv", from the table directory. The key must not contain a path
+// separator character; if it does, os.ErrInvalid will be returned. If the file
+// does not exist, os.ErrNotExist will be returned. For concurrency safetey, Get
+// acquires a shared file system lock on the file before reading its contents.
+// If the file has an exclusive lock on it, Get will spend up to a millisecond
+// waiting for the shared lock before returning a context.DeadlineExceeded
+// error.
 func (table *Table) Get(key string) ([]byte, error) {
 	// Make sure there is no directory separator.
 	if strings.ContainsRune(key, os.PathSeparator) {
@@ -127,9 +129,9 @@ func (table *Table) Get(key string) ([]byte, error) {
 	return val, nil
 }
 
-// Set sets the value for the key by writing it to the file named for key in the
-// table directory. The key must not contain a path separator character; if it
-// does, os.ErrInvalid will be returned.
+// Set sets the value for the key by writing it to the file named for key, plus
+// the extension ".kv", in the table directory. The key must not contain a path
+// separator character; if it does, os.ErrInvalid will be returned.
 //
 // To set the value, Set first creates a temporary file with the key name, plus
 // ".tmp" and the PID appended to it, and tries to acquire an exclusive lock. If
@@ -138,9 +140,8 @@ func (table *Table) Get(key string) ([]byte, error) {
 // error. Once it has the lock, it writes the value to the temporary file.
 //
 // Next, it tries to acquire an exclusive lock on the file with the key name,
-// again waiting  up to a millisecond before returning a
-// context.DeadlineExceeded error. Once it has the lock, it moves the temporary
-// file to the new file.
+// again waiting up to a millisecond before returning a context.DeadlineExceeded
+// error. Once it has the lock, it moves the temporary file to the new file.
 func (table *Table) Set(key string, value []byte) error {
 	// Make sure there is no directory separator.
 	if strings.ContainsRune(key, os.PathSeparator) {
@@ -189,13 +190,13 @@ func (table *Table) Set(key string, value []byte) error {
 	return os.Rename(tmp, file)
 }
 
-// Delete deletes the key and its value by deleting the file named for key in
-// the table directory.  The key must not contain a path separator character; if
-// it does, os.ErrInvalid will be returned. Before deleting the file, Delete
-// tries to acquire an exclusive lock. If the file already has exclusive lock,
-// Delete will wait up to a millisecond to acquire the lock before returning a
-// context.DeadlineExceeded error. Once it has acquired the lock, it deletes the
-// file.
+// Delete deletes the key and its value by deleting the file named for key, plus
+// the extension ".kv", in the table directory. The key must not contain a path
+// separator character; if it does, os.ErrInvalid will be returned. Before
+// deleting the file, Delete tries to acquire an exclusive lock. If the file
+// already has exclusive lock, Delete will wait up to a millisecond to acquire
+// the lock before returning a context.DeadlineExceeded error. Once it has
+// acquired the lock, it deletes the file.
 func (table *Table) Delete(key string) error {
 	// Make sure there is no directory separator.
 	if strings.ContainsRune(key, os.PathSeparator) {
