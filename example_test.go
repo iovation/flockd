201dbd078441b2c6f1687e10a303603c91130f2f
David E. Wheeler <david.wheeler@iovation.com>
2018-01-29 15:26:58 -0500

    Add full example.

---
+++ example_test.go
@@ -0,0 +1,53 @@
+package flockd_test
+
+import (
+	"fmt"
+	"github.com/theory/flockd"
+	"log"
+	"os"
+	"time"
+)
+
+var timeout = 10 * time.Millisecond
+
+func Example() {
+	// Set up a new database.
+	db, err := flockd.New("flockd.db", timeout)
+	if err != nil {
+		log.Fatal("flockd.New", err)
+	}
+	defer os.Remove("flockd.db")
+
+	// Create a table.
+	tbl, err := db.Table("foo")
+	if err != nil {
+		log.Fatal("flockd.Table", err)
+	}
+
+	// Store a key/value pair in the table.
+	key := "greeting"
+	if err := tbl.Set(key, []byte("Hello world!")); err != nil {
+		log.Fatal("flockd.Set", err)
+	}
+
+	// Fetch the value.
+	val, err := tbl.Get(key)
+	if err != nil {
+		log.Fatal("flockd.Get", err)
+	}
+	fmt.Println(string(val))
+
+	// Delete the value.
+	if err := tbl.Delete(key); err != nil {
+		log.Fatal("flockd.Delete", err)
+	}
+
+	// No more value.
+	val, err = tbl.Get(key)
+	if err != os.ErrNotExist {
+		log.Fatal("flockd.Get", err)
+	}
+
+	// Output:
+	// Hello world!
+}
